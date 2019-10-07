5d6baf78b0f50b88a7f9068fdaddb6bf2ce8e0b3
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-10-07 12:32:56 +0100

    Added Get and Put methods

--- src/Assignment1/LowestCommonAncestor.java
+++ src/Assignment1/LowestCommonAncestor.java
@@ -5,12 +5,12 @@ public class LowestCommonAncestor <Key extends Comparable<Key>, Value>{
 	
 	public class Node{
 		public Key key;
-		public Value val;
-		public Node left, right;
+		public Value value;
+		public Node leftSide, rightSide;
 		public int N;
 		
-		public Node(Key key, Value val){
-			this.val = val;
+		public Node(Key key, Value value){
+			this.value = value;
 			this.key = key;
 		}
 	}
@@ -28,8 +28,56 @@ public class LowestCommonAncestor <Key extends Comparable<Key>, Value>{
 			{ 
 			  if (node == null) return(0); 
 			  else { 
-			    return(size(node.left) + 1 + size(node.right)); 
+			    return(size(node.leftSide) + 1 + size(node.rightSide)); 
 			  } 
 			} 
+			//Put method, inserts value into tree
+			public void put(Key key, Value value){
+				root = put(root, key, value);
+			}
+			
+			private Node put(Node x, Key key, Value value){
+				if(x == null)
+				{
+					return new Node(key, value);
+				}
+				int comp = key.compareTo(x.key);
+				if(comp < 0)
+				{
+					x.leftSide = put(x.leftSide, key, value);
+				}
+				else if(comp > 0)
+				{
+					x.rightSide = put(x.rightSide, key, value);
+				}else
+				{
+					x.value = value;
+				}
+				x.N = 1 + size(x.leftSide) + size(x.rightSide);
+				return x;
+		 	}
+			
+			//Get method, search by key order
+
+			public Value get(Key key){
+				Node x = root;
+				
+				while(x != null){
+					
+					int comp = key.compareTo(x.key);
+					if(comp < 0)
+					{
+						x = x.leftSide;
+					}
+					else if(comp > 0)
+					{
+						x = x.rightSide;
+					}
+					else{
+						return x.value;
+					}
+				}
+				return null;
+			}
 
 }
