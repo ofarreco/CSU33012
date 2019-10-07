ae85b0bd1d3bc81f7ac23a144cd632f256da6d21
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-10-07 17:50:36 +0100

    Created testing classes - nothing within them yet

--- src/Assignment1/LowestCommonAncestorTest.java
+++ src/Assignment1/LowestCommonAncestorTest.java
@@ -1,14 +1,64 @@
 package Assignment1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 
 class LowestCommonAncestorTest {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testIsTreeEmpty()
+	{
+		
 	}
+	@Test
+	public void testForTreeSize1()
+	{
+		
+	}
+	@Test
+	public void testTwoNodes()
+	{
+		
+	}
+	@Test
+	public void testForNonExistingNodes()
+	{
+		
+	}
+	@Test
+	public void testAllSameNode(){
+		
+		
+		
+	}
+	@Test
+	public void testTreeSize(){
+		
+	}
+	@Test 
+	public void testSameValue()
+	{
+		
+	}
+	@Test
+	public void testCommonAncestor(){
+		
+		
 
+	}
+	@Test
+	public void testGet(){
+		
+	}
+	@Test
+	public void testFullTree()
+	{
+		
+		
+	}
 }
