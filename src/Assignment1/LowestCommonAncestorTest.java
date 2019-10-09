22ed5fca5ef351bb78e53e3e8130ddd8fffc335b
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-10-09 12:13:07 +0100

    Added tests for TreeEmpty, TreeSize, Get function, FullTree

--- src/Assignment1/LowestCommonAncestorTest.java
+++ src/Assignment1/LowestCommonAncestorTest.java
@@ -10,10 +10,63 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 
 class LowestCommonAncestorTest {
-	@Test
 	public void testIsTreeEmpty()
 	{
-		
+		LowestCommonAncestor<Integer, Integer> test = new LowestCommonAncestor<Integer, Integer>();
+		boolean isEmpty = test.isEmpty();
+		assertEquals(isEmpty, true);
+		test.put(3, 6);
+		test.put(6, 12);
+		test.put(9, 15);
+		test.put(12, 18);
+		isEmpty = test.isEmpty();
+		assertEquals(isEmpty, false);
+	}
+	@Test
+	public void testTreeSize()
+	{
+		LowestCommonAncestor<Integer, Integer> test = new LowestCommonAncestor<Integer, Integer>();
+		assertEquals(test.size(), 0);
+		test.put(1, 1);
+		assertEquals(test.size(), 1);
+		test.put(2, 4);
+		test.put(3, 6);
+		assertEquals(test.size(), 3);
+		test.put(1, 8);
+		assertEquals(test.size(), 3); //Size doesn't change as element already exists
+	}
+	
+	@Test
+	public void testGet()
+	{
+		LowestCommonAncestor<Integer, Integer> test = new LowestCommonAncestor<Integer, Integer>();
+		assertNull(test.get(3));
+		test.put(3, 1);
+		assertSame(test.get(3), 1);
+		test.put(4,2);
+		test.put(5, 3);
+		test.put(6, 4);
+		assertSame(test.get(5), 3);
+		assertNull(test.get(3));
+	}
+	@Test
+	public void testFullTree()
+	{
+		LowestCommonAncestor<Integer, Integer> test = new LowestCommonAncestor<Integer, Integer>();
+		test.put(1, 11);
+		test.put(2, 10);
+		test.put(3, 9);
+		test.put(4, 8);
+		test.put(5, 7);
+		test.put(6, 6);
+		test.put(7, 5);
+		test.put(8, 4);
+		test.put(9, 3);
+		test.put(10, 2);
+		test.put(11, 1);
+		assertSame(test.search(test.root,18,1).key,8);
+		assertSame(test.search(test.root,18,7).key,8);
+		assertSame(test.search(test.root,12,16).key,14);	
 	}
 	@Test
 	public void testForTreeSize1()
@@ -31,16 +84,11 @@ class LowestCommonAncestorTest {
 		
 	}
 	@Test
-	public void testAllSameNode(){
-		
-		
-		
-	}
-	@Test
-	public void testTreeSize(){
+	public void testAllSameNode()
+	{	
 		
 	}
-	@Test 
+@Test 
 	public void testSameValue()
 	{
 		
@@ -50,15 +98,5 @@ class LowestCommonAncestorTest {
 		
 		
 
-	}
-	@Test
-	public void testGet(){
-		
-	}
-	@Test
-	public void testFullTree()
-	{
-		
-		
 	}
 }
