02ca6289a0fef76fd2565590087981774d502346
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-10-15 14:48:48 +0100

    Create DirectedAcyclicGraphTest.java

---
+++ src/Assignment1/DirectedAcyclicGraphTest.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class DirectedAcyclicGraphTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
