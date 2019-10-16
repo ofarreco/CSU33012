8fd403b34f51518628bb8d8825aad36a6b1e5e3d
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-10-16 12:46:26 +0100

    Update DirectedAcyclicGraphTest.java
    Added testDirectedAcyclicGraph

--- src/Assignment1/DirectedAcyclicGraphTest.java
+++ src/Assignment1/DirectedAcyclicGraphTest.java
@@ -8,8 +8,21 @@ import org.junit.runners.JUnit4;
 public class DirectedAcyclicGraphTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDirectedAcyclicGraph()
+	{
+		DirectedAcyclicGraph graph = new DirectedAcyclicGraph(10);
+		
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(4, 6);
+		graph.addEdge(4, 7);
+		
+		assertEquals(1, graph.indeg(4));
+		assertEquals(2, graph.outdeg(4));
+		assertEquals(5, graph.edge());
+		assertEquals(10, graph.X());
+		String adjacent = "[6, 7]";
+		assertEquals(adjacent, graph.adjacent(4).toString());
 	}
-
 }
